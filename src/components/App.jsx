0b6d3d72b691722a19b5fe30ea597bf6270eb057
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-12-30 00:08:30 +0200

    ex1 hw3 finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,32 @@ import { Container } from './App.styled';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
 import { Filter } from './Filter';
+import data from './Data/data.json';
+
+const TEL = 'contacts';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    // console.log('componentDidMount');
+    const contacts = JSON.parse(localStorage.getItem(TEL)) || data;
+    this.setState({ contacts });
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (
+      prevState.contacts.length !== 0 &&
+      prevState.contacts.length !== contacts.length
+    ) {
+      localStorage.setItem(TEL, JSON.stringify(this.state.contacts));
+      // console.log('componentDidUpdate');
+    }
+  }
 
   addContact = newPhoneNumber => {
     const { name, number } = newPhoneNumber;
